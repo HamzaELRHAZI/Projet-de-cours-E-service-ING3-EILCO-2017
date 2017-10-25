1968016e0386f59cb81d62269cb98c625ccf5cb5
HAMZA ELRHAZI <h.elrhazi@enim.ac.ma>
2017-10-25 08:49:04 +0200

    upload project from session 25 october 2017

---
+++ HelloEJBProject/ejbModule/fr/eilco/ing/bean/HelloBean.java
@@ -0,0 +1,40 @@
+package fr.eilco.ing.bean;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema="database01", name="hellotable")
+
+public class HelloBean implements Serializable{
+	private  static final long seriaVersionUID= 1L;
+	private int id;
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Id
+	@GeneratedValue
+	@Column(name="pk_hello")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+		}
+	
+	
+	
+
+	
+}
---
+++ HelloWorldProject2/src/fr/eilco/ing/HelloServlet.java
@@ -0,0 +1,46 @@
+package fr.eilco.ing;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/Hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		//out.println("HelloBasma");
+		//this.getServletContext().getRequestDispatcher( "/WEB-INF/Hello.jsp" ).forward( request, response );
+		//response.sendRedirect("testSendRedirect.html");
+		response.sendRedirect("WEB-INF/Hello.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
